715f45fe0b87e5465b11dcfbb278feedd9308380
unknown <shamil.mail14@gmail.com>
2024-02-16 19:02:09 +0200

    Fixed mistakes HM 4

--- HW 4. Конвертор.js
+++ HW 4. Конвертор.js
@@ -1,39 +1,15 @@
-
-let quantity = 5;
-let unit = " km";
-let distanceKM =  quantity + unit; 
-if (distanceKM === "5 km") {
-    let distanceM = "5000 m";
-    let resultOfDistance = "5 km its 5000 m"
-    let hour = "часы"
-    let minutes = "минуты"
-    let kilograms = "килограммы"
-    let milligrams = "миллиграммы"
-}  
-switch("5 km its 5000 mв") {
-    case "часы":
-        console.log("часы") ;
+let quantity = 1;
+let unit = "km"
+switch(unit) {
+    case "km":
+        console.log("1 километр это " + 1000 * quantity + " метров");
+        break; 
+        case("h"):
+        console.log("1 час это " + 60 * quantity + " минут");
         break;
-    case "минуты":
-        console.log("минуты");
-        break;
-    case "килограммы":
-        console.log("килограммы");
-        break;
-    case "миллиграммы":
-        console.log("миллиграммы");
-        break;
-    case "5":
-        console.log("просто цифра");
-        break;
-    case " km":
-        console.log("просто кг");
-        break;      
-    case "5 km its 5000 m":
-        console.log("5 км это 5000 метров");
-        break;
+        case("kg"):
+        console.log("1 килограмм это " + 1000 * quantity + " миллиграмм");
+        break; 
         default:
-            console.log("введите правильный вариант");
-                    
-        
+            console.log("Введите правильный вариант");
 }
